6fb3ecb9291f42062c92e8dfb8d3ff5f4a1a2c7f
Kyle Aquino <kyleaquino1@gmail.com>
2020-02-13 03:19:55 -0800

    Added StatsVC and ManagementVC
    - Connected Segues to the buttons in MainVC

--- ios/ParkInn/ParkInn/Controllers/MainVC.swift
+++ ios/ParkInn/ParkInn/Controllers/MainVC.swift
@@ -7,13 +7,34 @@
 //
 
 import UIKit
+import MaterialComponents
 
 class MainVC: UIViewController {
+
+    @IBOutlet weak var statsButton: MDCButton!
+    @IBOutlet weak var managementButton: MDCButton!
+
+    private let cornerRadius: CGFloat = 12.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        roundCorners()
     }
 
+    private func roundCorners() {
+        statsButton.layer.cornerRadius = self.cornerRadius
+        managementButton.layer.cornerRadius = self.cornerRadius
+    }
+
+    // MARK: - Segues
+
+    @IBAction func openStatsVC(_ sender: Any) {
+        performSegue(withIdentifier: "toStatsVC", sender: nil)
+    }
+
+    @IBAction func openManagementVC(_ sender: Any) {
+        performSegue(withIdentifier: "toManagementVC", sender: nil)
+    }
 }
---
+++ ios/ParkInn/ParkInn/Controllers/ManagementVC.swift
@@ -0,0 +1,19 @@
+//
+//  ManagementVC.swift
+//  ParkInn
+//
+//  Created by Kyle Aquino on 2/13/20.
+//  Copyright © 2020 ParkInn. All rights reserved.
+//
+
+import UIKit
+
+class ManagementVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
