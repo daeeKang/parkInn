7ec4c73a0792e2308722222214fd9dfe89788f1f
Kyle Aquino <kyleaquino1@gmail.com>
2020-02-12 23:17:49 -0800

    Added subfolders to follow MVC
    - Created MainVC and linked to storyboard

---
+++ ios/ParkInn/ParkInn/Controllers/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  MainVC.swift
+//  ParkInn
+//
+//  Created by Kyle Aquino on 2/12/20.
+//  Copyright © 2020 ParkInn. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
