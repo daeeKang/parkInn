382953ee3c945de70bc7034310d1a76f343a8236
Lichelle Gaerlan <litchi@Lichelles-MacBook-Pro.local>
2020-02-18 14:08:54 -0800

    added material-ui icons

--- front-end/park-inn/src/App.js
+++ front-end/park-inn/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button/Button';
+import { Button, ListItem} from '@material-ui/core'
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import HomeIcon from '@material-ui/icons/Home';
 
 class App extends Component {
   render() {
     return (
       <div>
         Park Inn App
+       
         <Button> Hi </Button>
+
+        <ListItem>
+          <ListItemIcon><HomeIcon/></ListItemIcon>
+        </ListItem>
       </div>
     );
   }
