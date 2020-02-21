9fe8c2aeeb1faede04d43d5ece30710be7563ce9
Kyle Aquino <kyleaquino1@gmail.com>
2020-02-21 00:00:21 -0800

    Revert "Merge branch 'master' into makeSideMenu-ios-JH"
    This reverts commit 6e1208721e95af2258665882851d4b10198c6b88, reversing
    changes made to 3f1773edc2ea47bc473291b62e24fcc9d7dd9640.

--- back-end/model/model.js
+++
@@ -1,6 +0,0 @@
-const Company = require('./Company');
-const Lot = require('./Lot');
-const User = require('./User');
-const Customer = require('./Customer');
-
-module.exports = {User, Company, Lot, Customer};
--- back-end/server.js
+++ back-end/server.js
@@ -1,32 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const Mongoose = require('mongoose');
-const app = express();
-const dotenv = require('dotenv');
-const port = 3000;
+//run 'nodemon' for continuous build :-) 
+const express = require('express')
+const app = express()
+const port = 3000
 
-//initial set up
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
-dotenv.config();
+app.get('/', (req, res) => res.send('is running dude'))
 
-//bring in routes
-const companyRoute =  require('./routes/Company');
-const lotRoute =      require('./routes/Lot');
-const customerRoute = require('./routes/Customer');
-
-//tell app to use brought in routes
-app.use('/Company', companyRoute);
-app.use('/Lot', lotRoute);
-app.use('/Customer', customerRoute);
-
-//connect to mongodb
-//if this isn't working, you don't have a .env file saved in this directory
-Mongoose.connect(
-  process.env.MONGO_URI
-)
-
-app.listen(port, () => console.log(`dab on em we listenin on port ${port}`));
-
-//example of connecting to server
-
+app.listen(port, () => console.log(`dab on em we listenin on port ${port}`))
