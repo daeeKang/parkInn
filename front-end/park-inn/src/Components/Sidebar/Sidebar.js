4096d71b3f78701fb4dd8da2d7277920934944ac
daee kang <44410043+daeeKang@users.noreply.github.com>
2020-02-25 17:55:30 -0800

    Menu bar closes properly now :+1:

--- front-end/park-inn/src/Components/Sidebar/Sidebar.js
+++ front-end/park-inn/src/Components/Sidebar/Sidebar.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { slide as Menu } from 'react-burger-menu';
+import { stack as Menu } from 'react-burger-menu';
 import './Sidebar.css';
 import { Link } from 'react-router-dom';
 
-export default props => {
-    return (
-        <Menu>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/statistics'>Statistics</Link></li>
-        </Menu>
-    )
+class Sidebar extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            menuOpen: false
+        }
+    }
+
+    handleStateChange = state => {
+        this.setState({menuOpen: state.isOpen});
+    }
+
+    closeMenu = () => {
+        this.setState({menuOpen: false});
+    }
+
+    render() {
+        return (
+            <Menu
+                isOpen={this.state.menuOpen}
+                onStateChange={state => this.handleStateChange(state)}
+            >
+                <li><Link onClick={this.closeMenu} to='/'>Home</Link></li>
+                <li><Link onClick={this.closeMenu} to='/statistics'>Statistics</Link></li>
+            </Menu>
+        )
+    }
 }
+
+export default Sidebar;
