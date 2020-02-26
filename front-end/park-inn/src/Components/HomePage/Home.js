e1d806d1ce747d09a9f3e396af10b34be9a5389a
daee kang <44410043+daeeKang@users.noreply.github.com>
2020-02-26 02:23:08 -0800

    added log in page

--- front-end/park-inn/src/Components/HomePage/Home.js
+++ front-end/park-inn/src/Components/HomePage/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Login from '../LoginPage/Login' //just place holder for now maybe lol
 
 export default props => {
     return (
         <div>
-            test hello
+            <Login/>
         </div>
     )
 }
