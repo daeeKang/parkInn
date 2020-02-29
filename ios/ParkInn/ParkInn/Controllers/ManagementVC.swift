468ded37707967c43c19d39cb3029ec7070a856a
leonbatista <55902667+leonbatista@users.noreply.github.com>
2020-02-28 19:41:12 -0800

    collection view into two columns

--- ios/ParkInn/ParkInn/Controllers/ManagementVC.swift
+++ ios/ParkInn/ParkInn/Controllers/ManagementVC.swift
@@ -11,13 +11,19 @@ import UIKit
 //classe to manage cell structure
 class ManagementVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
      //test data for
      let lotNumber = ["Lot 1", "Lot 2", "Lot 3", "Lot 4", "Lot 5", "Lot 6", "Lot 7", "Lot 8", "Lot 9"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //set up the amount of column
+        let width = (view.frame.size.width - 20) / 2
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width)
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -29,6 +35,7 @@ class ManagementVC: UIViewController, UICollectionViewDataSource, UICollectionVi
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "manageCell", for: indexPath) as! ManagementCVCell
         
         cell.testLabel.text = lotNumber[indexPath.item]
+        //cell.backgroundColor = UIColor.white
         
         return cell
         
