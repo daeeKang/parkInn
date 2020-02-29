fe38756b027926c40225a85b1c01ea6a0d01a2f6
leonbatista <55902667+leonbatista@users.noreply.github.com>
2020-02-28 20:37:19 -0800

    finished manage collection view layout

--- ios/ParkInn/ParkInn/Controllers/ManagementVC.swift
+++ ios/ParkInn/ParkInn/Controllers/ManagementVC.swift
@@ -13,19 +13,24 @@ class ManagementVC: UIViewController, UICollectionViewDataSource, UICollectionVi
 
     @IBOutlet private weak var collectionView: UICollectionView!
     
-     //test data for
+     //test data
      let lotNumber = ["Lot 1", "Lot 2", "Lot 3", "Lot 4", "Lot 5", "Lot 6", "Lot 7", "Lot 8", "Lot 9"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //set up the amount of column
+        //set up the amount of columns
         let width = (view.frame.size.width - 20) / 2
+        //variable layout to access properties of collectionView
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        //size of each cell
         layout.itemSize = CGSize(width: width, height: width)
+        //space for all corners of the cell
+        layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
         
     }
     
+    //get number of items in the array
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
            return lotNumber.count
     }
@@ -34,9 +39,16 @@ class ManagementVC: UIViewController, UICollectionViewDataSource, UICollectionVi
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "manageCell", for: indexPath) as! ManagementCVCell
         
+        //center text in the center
+        cell.testLabel.textAlignment = NSTextAlignment.center
+   
+        //change label text for testing
         cell.testLabel.text = lotNumber[indexPath.item]
-        //cell.backgroundColor = UIColor.white
-        
+        //round out border 
+        cell.contentView.layer.cornerRadius = 40
+        cell.contentView.layer.borderWidth = 2
+       
+
         return cell
         
     }
