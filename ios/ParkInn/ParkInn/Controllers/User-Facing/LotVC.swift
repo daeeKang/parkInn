5ba4e7035a7e57ea87225677bc56f59bf3d75a7e
Kyle Aquino <kyleaquino1@gmail.com>
2020-03-01 12:49:27 -0800

    Added a LotVC file to display lots to the user
    - Created a User-Facing group to distinguish the user facing view controllers from the management controllers

---
+++ ios/ParkInn/ParkInn/Controllers/User-Facing/LotVC.swift
@@ -0,0 +1,30 @@
+//
+//  LotVC.swift
+//  ParkInn
+//
+//  Created by Kyle Aquino on 3/1/20.
+//  Copyright © 2020 ParkInn. All rights reserved.
+//
+
+import UIKit
+
+class LotVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
