1b6923929ddad8f16368c3292fe797e80d2ab199
daee kang <44410043+daeeKang@users.noreply.github.com>
2020-03-03 16:57:43 -0800

    added drawing

--- front-end/park-inn/src/Components/Renderer/Renderer.js
+++ front-end/park-inn/src/Components/Renderer/Renderer.js
@@ -1,41 +1,47 @@
 import React from 'react';
 import Konva from 'konva';
 import './Renderer.css'
-import { Stage, Layer, Star, Text, Line } from 'react-konva';
+import { Stage, Layer, Star, Text, Line, Rect } from 'react-konva';
 
 export default class Renderer extends React.Component {
     state = {
         scale: 1,
+        isDrawing: true,
+        isPan: false,
         stage: {
             draggable: true,
             x: window.innerWidth/2,
             y: window.innerHeight/2
         },
+        walls: [
+                       
+        ], 
         stars: [
-            //this is just filler for now 
-            //TO-DO: REMOVE THIS
-            {
-                x: Math.random() * window.innerWidth,
-                y: Math.random() * window.innerHeight
-            },
-            {
-                x: Math.random() * window.innerWidth,
-                y: Math.random() * window.innerHeight
-            },
-            {
-                x: Math.random() * window.innerWidth,
-                y: Math.random() * window.innerHeight
-            },
-            {
-                x: Math.random() * window.innerWidth,
-                y: Math.random() * window.innerHeight
-            },
-            {
-                x: Math.random() * window.innerWidth,
-                y: Math.random() * window.innerHeight
-            },
+            // //this is just filler for now 
+            // //TO-DO: REMOVE THIS
+            // {
+            //     x: Math.random() * window.innerWidth,
+            //     y: Math.random() * window.innerHeight
+            // },
+            // {
+            //     x: Math.random() * window.innerWidth,
+            //     y: Math.random() * window.innerHeight
+            // },
+            // {
+            //     x: Math.random() * window.innerWidth,
+            //     y: Math.random() * window.innerHeight
+            // },
+            // {
+            //     x: Math.random() * window.innerWidth,
+            //     y: Math.random() * window.innerHeight
+            // },
+            // {
+            //     x: Math.random() * window.innerWidth,
+            //     y: Math.random() * window.innerHeight
+            // },
         ],
     }
+
 
     //EVENT HANDLERS DOWN BELOOOOWW-----------------------------------------------------------------------------------------------------------------
     //this is star stuff we will remove this
@@ -48,6 +54,9 @@ export default class Renderer extends React.Component {
             scaleX: 1.1,
             scaleY: 1.1
         });
+        this.setState({
+            isPan: false
+        })
     };
     handleDragEnd = e => {
         e.target.to({
@@ -61,32 +70,15 @@ export default class Renderer extends React.Component {
     };
     //stage handeling
     handleStageDrag = e => {
-        if (this.state.stage.draggable)
+        if (this.state.isPan){
             this.setState({
                 stage: {
                     draggable: true,
                     x: e.target.attrs.x,
                     y: e.target.attrs.y
                 }
-            })
-    }
-    resetOrigin = e => {
-        this.setState({
-            stage: {
-                draggable: true,
-                x: 0,
-                y: 0
-            }
-        })
-    }
-    moveStage = e => {
-        this.setState({
-            stage: {
-                draggable: !this.state.stage.draggable,
-                x: this.state.stage.x,
-                y: this.state.stage.y
-            }
-        })
+            });
+        }
     }
     stageZoom = e => {
         //this is code copied from stack overflow dont ask me how it works lol
@@ -112,6 +104,68 @@ export default class Renderer extends React.Component {
         });
     }
 
+    isPaint = false;
+    startDrawing = e => {
+        //create rectangle
+        if(!this.state.isDrawing) return;
+        
+        if(this.isPaint){
+            this.isPaint = false;
+            return;
+        }
+
+        let walls = this.state.walls;
+        walls.push({
+            x: e.evt.layerX - this.state.stage.x,
+            y: e.evt.layerY - this.state.stage.y,
+            width: 0,
+            height: 0
+        })
+        this.setState({
+            walls: walls
+        })
+        console.log(walls);
+        this.isPaint = true;
+    }
+    sizeDrawing = e => {
+        if(!this.state.isDrawing || !this.isPaint) return;
+        
+        let walls = this.state.walls;
+        let wall = walls[walls.length - 1];
+        wall.width = e.evt.layerX - this.state.stage.x - wall.x;
+        wall.height = e.evt.layerY - this.state.stage.y - wall.y;
+        
+        this.setState({
+            walls: walls
+        });
+    }
+    //---------------------------BUTTON TYPA TINGZ--------------------------------//
+    resetOrigin = e => {
+        this.setState({
+            stage: {
+                draggable: true,
+                x: 0,
+                y: 0
+            }
+        })
+    }
+    toggleMoveStage = e => {
+        this.setState({
+            isPan: !this.state.isPan,
+            isDrawing: false,
+            stage: {
+                x: this.state.stage.x,
+                y: this.state.stage.y
+            }
+        })
+    }
+    toggleDrawingMode = e => {
+        this.setState({
+            isDrawing: !this.state.isDrawing,
+            isPan: false
+        });
+    }
+
 
     //-----------------------------RENDER---:-)-------------------------kill me-----------------------//
     render() {
@@ -121,22 +175,28 @@ export default class Renderer extends React.Component {
                     <p>
                         {this.state.stage.x}, {this.state.stage.y}
                     </p>
-                    <button onClick={this.resetOrigin}>
+                    <button onClick={this.resetOrigin} >
                         reset origin
                     </button>
-                    <button onClick={this.moveStage}>
+                    <button onClick={this.toggleMoveStage}>
                         move stage
+                    </button>
+                    <button onClick={this.toggleDrawingMode}>
+                        drawing mode 
                     </button>
                 </div>
 
                 <Stage width={window.innerWidth} height={window.innerHeight}
-                    draggable={this.state.stage.draggable}
+                    draggable={this.state.isPan}
                     x={this.state.stage.x}
                     y={this.state.stage.y}
-                    onDragEnd={this.handleStageDrag}
-                    onWheel={this.stageZoom}
                     scaleX={this.state.scale}
                     scaleY={this.state.scale}
+
+                    onMouseDown={this.startDrawing}
+                    onMouseMove={this.sizeDrawing}
+                    onDragEnd={this.handleStageDrag}
+                    onWheel={this.stageZoom}
                 >
                     <Layer>
                         {[...Array(200)].map((_, i) => (
@@ -161,6 +221,18 @@ export default class Renderer extends React.Component {
                                 listening={false}
                             />
                         ))}
+                    </Layer>
+
+                    <Layer>
+                        {this.state.walls.map((wall, i) => {
+                            return <Rect
+                                x={wall.x}
+                                y={wall.y}
+                                width={wall.width}
+                                height={wall.height}
+                                fill="black"
+                            />
+                        })}
                     </Layer>
                     <Layer>
                         {this.state.stars.map((star, i) => (
