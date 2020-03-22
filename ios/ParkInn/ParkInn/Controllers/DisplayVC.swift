a2f6e99b33d612acf2a57ec69c729693dd6cdbbb
leonbatista <55902667+leonbatista@users.noreply.github.com>
2020-03-22 16:32:16 -0700

    Added a new file to use view controller

---
+++ ios/ParkInn/ParkInn/Controllers/DisplayVC.swift
@@ -0,0 +1,34 @@
+//
+//  DisplayVC.swift
+//  ParkInn
+//
+//  Created by Leonardo Batista on 3/22/20.
+//  Copyright © 2020 ParkInn. All rights reserved.
+//
+
+import UIKit
+
+class DisplayVC: UIViewController {
+
+    @IBOutlet weak var showImage: UIImageView!
+    
+    var image = UIImage()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
