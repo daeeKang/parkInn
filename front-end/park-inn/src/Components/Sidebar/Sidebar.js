4ca5e083ecf3bd59cd5dab2a2dedf37bed6002ef
awexis :3 <aolaes@chirotouch.com>
2020-03-22 19:14:21 -0700

    sidebar lint????

--- front-end/park-inn/src/Components/Sidebar/Sidebar.js
+++ front-end/park-inn/src/Components/Sidebar/Sidebar.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { stack as Menu } from "react-burger-menu";
-import "./Sidebar.css";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { stack as Menu } from 'react-burger-menu';
+import './Sidebar.css';
+import { NavLink } from 'react-router-dom';
 
 class Sidebar extends React.Component {
   constructor(props) {
@@ -19,47 +19,23 @@ class Sidebar extends React.Component {
     this.setState({ menuOpen: false });
   };
 
-  render() {
-    return (
-      <Menu
-        isOpen={this.state.menuOpen}
-        onStateChange={state => this.handleStateChange(state)}
-      >
-        <ul>
-          <li>
-            <NavLink className="link" onClick={this.closeMenu} to="/">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="link" onClick={this.closeMenu} to="/">
-              Manage Parking
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="link" onClick={this.closeMenu} to="/statistics">
-              Statistics
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="link" onClick={this.closeMenu} to="/events">
-              Event Calendar
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="link" onClick={this.closeMenu} to="/incidents">
-              Manage Incidents
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="link" onClick={this.closeMenu} to="/management">
-              Management Settings
-            </NavLink>
-          </li>
-        </ul>
-      </Menu>
-    );
-  }
+    render() {
+        return (
+            <Menu
+                isOpen={this.state.menuOpen}
+                onStateChange={state => this.handleStateChange(state)}
+            >
+                <ul>
+                    <li><NavLink className="link" onClick={this.closeMenu} to='/'>Home</NavLink></li>
+                    <li><NavLink className="link" onClick={this.closeMenu} to='/'>Manage Parking</NavLink></li>
+                    <li><NavLink className="link" onClick={this.closeMenu} to='/statistics'>Statistics</NavLink></li>
+                    <li><NavLink className="link" onClick={this.closeMenu} to='/events'>Event Calendar</NavLink></li>
+                    <li><NavLink className="link" onClick={this.closeMenu} to='/incidents'>Manage Incidents</NavLink></li>
+                    <li><NavLink className="link" onClick={this.closeMenu} to='/management'>Management Settings</NavLink></li>
+                </ul>
+            </Menu>
+        )
+    }
 }
 
 export default Sidebar;
