cde0c0ed4483c67632c87844632766da7add294d
awexis :3 <aolaes@chirotouch.com>
2020-03-22 19:10:27 -0700

    add employee page

--- front-end/park-inn/src/Components/ManagementPage/AddEmployee.js
+++ front-end/park-inn/src/Components/ManagementPage/AddEmployee.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import "./AddEmployee.css";
+import AddIcon from "../../Icons/AddIcon";
 
 export class AddEmployee extends Component {
   render() {
     return (
       <div class="page add-page">
         <div class="header">Add Employee</div>
-        <div class="add-container"></div>
+        <div class="add-container">
+          <AddIcon></AddIcon>
+        </div>
       </div>
     );
   }
