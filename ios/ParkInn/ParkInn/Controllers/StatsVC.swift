58d930d9572ca990151c841ce71a5d87d8ddb05d
John Hanifzai <32753088+hanifzaijohn@users.noreply.github.com>
2020-03-29 21:07:36 -0700

    update

--- ios/ParkInn/ParkInn/Controllers/StatsVC.swift
+++ ios/ParkInn/ParkInn/Controllers/StatsVC.swift
@@ -19,25 +19,39 @@ class StatsVC: UIViewController {
 
 
         lots = ["Freemont St", "Las Vegas Strip", "Home 3 Space", "Lake Mead Front"]
-        let unitsSold:[Double] = [100, 302, 2, 28]
                 
-        setChart(dataPoints: lots, values: unitsSold)
+        setChart()
     }
     
-    func setChart(dataPoints: [String], values: [Double]) {
+    func setChart() {
         barChartView.noDataText = "You need to provide data for the chart."
-        
-        var dataEntries: [BarChartDataEntry] = []
-                
-        for i in 0..<dataPoints.count {
-            let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
-            dataEntries.append(dataEntry)
-        }
-                
-        let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Lots Reserved")
-        let chartData = BarChartData(xVals: lots, dataSet: chartDataSet)
-        barChartView.data = chartData
+
+            let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
             
+            let test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+            
+            var dataEntries: [BarChartDataEntry] = []
+            
+            for i in 0..<lots.count
+           {
+                let dataEntry = BarChartDataEntry(x: Double(test[i]), y: Double(unitsSold[i]))
+                
+                dataEntries.append(dataEntry)
+            }
+            
+        let chartDataSet = BarChartDataSet(entries: dataEntries, label: "Reservation count")
+        let chartData = BarChartData(dataSet: chartDataSet)
+            
+            barChartView.data = chartData
+        
+        barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
+        
+        chartDataSet.colors = [.white]
+
+        barChartView.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1)
+        
+        }
+
     }
 
-}
+
