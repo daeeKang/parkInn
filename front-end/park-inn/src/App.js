f53caff4699a40ba96b49ca8ae6ad6607c9a8083
William Lu <luw1@unlv.nevada.edu>
2020-03-29 23:17:02 -0700

    Update App.js

--- front-end/park-inn/src/App.js
+++ front-end/park-inn/src/App.js
@@ -4,14 +4,13 @@ import Header from './Components/Header/Header';
 // import Sidebar from './Components/Sidebar/Sidebar';
 import Main from './Components/Main';
 import Footer from './Components/Footer/Footer';
-import Loading from './Components/Loading/Loading'
 import { useAuth0 } from './react-auth0-spa'
 
 const App = () => {
   const { loading } = useAuth0();
 
   if (loading) {
-    return <Loading />;
+    return <div>Loading...</div>;
   }
 
   return (
