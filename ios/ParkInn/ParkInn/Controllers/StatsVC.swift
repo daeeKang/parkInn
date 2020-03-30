cdbd3417d2a06ae51b6c95ab0ac45c6939076e60
John Hanifzai <32753088+hanifzaijohn@users.noreply.github.com>
2020-03-29 20:40:50 -0700

    changes

--- ios/ParkInn/ParkInn/Controllers/StatsVC.swift
+++ ios/ParkInn/ParkInn/Controllers/StatsVC.swift
@@ -7,13 +7,37 @@
 //
 
 import UIKit
+import Charts
 
 class StatsVC: UIViewController {
-
+    @IBOutlet weak var barChartView: BarChartView!
+    var lots: [String]!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
+
+        lots = ["Freemont St", "Las Vegas Strip", "Home 3 Space", "Lake Mead Front"]
+        let unitsSold:[Double] = [100, 302, 2, 28]
+                
+        setChart(dataPoints: lots, values: unitsSold)
+    }
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        barChartView.noDataText = "You need to provide data for the chart."
+        
+        var dataEntries: [BarChartDataEntry] = []
+                
+        for i in 0..<dataPoints.count {
+            let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+                
+        let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Lots Reserved")
+        let chartData = BarChartData(xVals: lots, dataSet: chartDataSet)
+        barChartView.data = chartData
+            
     }
 
 }
