8a385d7314a558e4719b9f7c5b161795be2d4cdb
stan <knezes1@unlv.nevada.edu>
2020-04-02 21:25:32 -0700

    adding fields to schema

--- back-end/model/reservation.js
+++ back-end/model/reservation.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let ReservationSchema = new Schema({
-
-    
+    companyid: {type: String, required: true},
+    lotid: {type: String, required: true},
+    spotid: {type: String, required: true},
+    starttime: {type: Date, required: true},
+    endtime: {type: Date, required: true},
+    username: {type: String, required: true}
 
 });
 
