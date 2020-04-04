97a145829e70e71dae6178afe18c3d448785cc0f
daee kang <44410043+daeeKang@users.noreply.github.com>
2020-04-03 17:20:01 -0700

    fixed merge error

--- back-end/routes/Lot.js
+++ back-end/routes/Lot.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const model = require('../model/Model');
-const checkJwt = require('../middleware/checkJwt');
+const router = require("express").Router();
+const model = require("../model/Model");
+const checkJwt = require("../middleware/checkJwt");
 
-router.post('/AddLot', checkJwt, async (req, res) => {
+router.post("/AddLot", checkJwt, async (req, res) => {
     const lot = new model.Lot({
         companyid: req.body.companyid,
         lotid: req.body.lotid,
@@ -12,30 +12,29 @@ router.post('/AddLot', checkJwt, async (req, res) => {
         availableSpots: req.body.spots.length, //initially everything is availale
         location: {
             latitude: req.body.lat,
-            longitude: req.body.long
-        }
+            longitude: req.body.long,
+        },
     });
-
     //if lotid exists for companyid already, give error
     const check = await model.Lot.find({
         companyid: req.body.companyid,
-        lotid: req.body.lotid
+        lotid: req.body.lotid,
     });
 
     if (check.length != 0) return res.send("lot already exists my guy");
 
     await lot
         .save()
-        .then(res => {
+        .then((res) => {
             console.log(res, `lot ${req.body.lotid} saved`);
         })
-        .catch(err => {
+        .catch((err) => {
             console.log(err);
         });
 
     return res.status(201).json({
         message: "Handling post request",
-        createdProduct: lot
+        createdProduct: lot,
     });
 });
 
@@ -43,17 +42,17 @@ router.post("/UpdateLotDesign", async (req, res) => {
     await model.Lot.findOneAndUpdate(
         {
             companyid: req.body.companyid,
-            lotid: req.body.lotid
+            lotid: req.body.lotid,
         },
         {
-            lotDesign: req.body.design
+            lotDesign: req.body.design,
         }
     );
     console.log("dab");
     res.send(
         await model.Lot.findOne({
             companyid: req.body.companyid,
-            lotid: req.body.lotid
+            lotid: req.body.lotid,
         })
     );
 });
@@ -62,104 +61,123 @@ router.get("/GetLotDesign", async (req, res) => {
     console.log(req.query);
     let found = await model.Lot.findOne({
         companyid: req.query.companyid,
-        lotid: req.query.lotid
+        lotid: req.query.lotid,
     });
     console.log(found);
     res.send(found.lotDesign);
 });
 
 //TODO- Fix this shit
-router.post('/UpdatePeakTimes', checkJwt, async(req, res) => {
-  await model.Lot.findOneAndUpdate({
-    companyid: req.body.companyid,
-    lotid: req.body.lotid
-  }, {
-    peakTimes: req.body.peakTimes
-  })
+router.post("/UpdatePeakTimes", checkJwt, async (req, res) => {
+    await model.Lot.findOneAndUpdate(
+        {
+            companyid: req.body.companyid,
+            lotid: req.body.lotid,
+        },
+        {
+            peakTimes: req.body.peakTimes,
+        }
+    );
 
-  res.send(model.Lot.findOne({
-    companyid: req.body.companyid,
-    lotid: req.body.lotid
-  }))
-})
+    res.send(
+        model.Lot.findOne({
+            companyid: req.body.companyid,
+            lotid: req.body.lotid,
+        })
+    );
+});
 
-router.patch('/UpdateSpot', checkJwt, async (req, res) => {
-  const lot = await model.Lot.find({
-    companyid: req.body.companyid,
-    lotid: req.body.lotid
-  });
+router.patch("/UpdateSpot", checkJwt, async (req, res) => {
+    const lot = await model.Lot.find({
+        companyid: req.body.companyid,
+        lotid: req.body.lotid,
+    });
 
+    if (lot.length == 0)
+        return res.send("no lot found, btw change these error messages lmao");
+    const spots = lot[0]["spots"]; //we are gauranteed that there is only one entry as lotid is unique
 
-  if(lot.length == 0) return res.send('no lot found, btw change these error messages lmao');
-  const spots = lot[0]["spots"]; //we are gauranteed that there is only one entry as lotid is unique
-  
-  let found = false; 
-  spots.map(spot => {
-    if(req.body.spotid == spot.spotid){
-      spot["active"] = req.body.updateValue;
-      found = true;
+    let found = false;
+    spots.map((spot) => {
+        if (req.body.spotid == spot.spotid) {
+            spot["active"] = req.body.updateValue;
+            found = true;
+        }
+        return spot;
+    });
+
+    if (found === false)
+        return res.send("no spot found, btw change these error messages lmao");
+    await model.Lot.findByIdAndUpdate(
+        lot[0]["_id"],
+        { spots: spots },
+        (err) => {
+            if (err) {
+                return res.send(err);
+            } else {
+                return res.send("Success, updated.");
+            }
+        }
+    );
+});
+
+router.get("/GetLots/:companyid", checkJwt, async (req, res) => {
+    const lots = await model.Lot.find({ companyid: req.params.companyid });
+    try {
+        return res.send(lots);
+    } catch (err) {
+        return res.status(500).send(err);
     }
-    return spot;
-  })
+});
 
-  if(found === false) return res.send('no spot found, btw change these error messages lmao');
-  await model.Lot.findByIdAndUpdate(
-    lot[0]["_id"], 
-    {spots: spots},
-    (err) => {
-      if(err) {
-        return res.send(err);
-      } else {
-        return res.send("Success, updated.");
-      }
-    })
-})
+router.get("/GetLot/:companyid/:lotid", checkJwt, async (req, res) => {
+    const lot = await model.Lot.find({
+        companyid: req.params.companyid,
+        lotid: req.params.lotid,
+    });
 
-router.get('/GetLots/:companyid', checkJwt, async (req, res) => { 
-  const lots = await model.Lot.find({companyid: req.params.companyid});
-  try{
-      return res.send(lots);
-  }
-  catch(err){
-      return res.status(500).send(err);
-  }
-})
+    if (lot.length == 0)
+        return res.send("no lot found, btw change these error messages lmao");
 
-router.get('/GetLot/:companyid/:lotid', checkJwt, async(req, res) => {
-  const lot = await model.Lot.find({companyid: req.params.companyid, lotid: req.params.lotid});
+    return res.send(lot[0]);
+});
 
-  if(lot.length == 0) return res.send('no lot found, btw change these error messages lmao');
+router.get("/GetAllSpots/:companyid/:lotid", checkJwt, async (req, res) => {
+    const lot = await model.Lot.find({
+        companyid: req.params.companyid,
+        lotid: req.params.lotid,
+    });
 
-  return res.send(lot[0]);
-})
+    if (lot.length == 0)
+        return res.send("no lot found, btw change these error messages lmao");
 
-router.get('/GetAllSpots/:companyid/:lotid', checkJwt, async(req, res) => {
-  const lot = await model.Lot.find({companyid: req.params.companyid, lotid: req.params.lotid});
+    res.send(lot[0].spots);
+});
 
-  if(lot.length == 0) return res.send('no lot found, btw change these error messages lmao');
+router.get("/GetParkingSpot/:companyid/:lotid", checkJwt, async (req, res) => {
+    const lot = await model.Lot.find({
+        companyid: req.params.companyid,
+        lotid: req.params.lotid,
+    });
 
-  res.send(lot[0].spots);
-})
+    //check if we found the lot
+    if (lot.length == 0)
+        return res.send("no lot found, btw change these error messages lmao");
 
-router.get('/GetParkingSpot/:companyid/:lotid', checkJwt, async(req, res) => {
-  const lot = await model.Lot.find({companyid: req.params.companyid, lotid: req.params.lotid});
-  
-  //check if we found the lot
-  if(lot.length == 0) return res.send('no lot found, btw change these error messages lmao');
+    const spots = lot[0]["spots"]; //we are gauranteed that there is only one entry as lotid is unique
 
-  const spots = lot[0]["spots"]; //we are gauranteed that there is only one entry as lotid is unique
-  
-  let out; 
-  spots.map(spot => {
-    if(req.body.spotid == spot.spotid){
-      out = spot;
-    }
+    let out;
+    spots.map((spot) => {
+        if (req.body.spotid == spot.spotid) {
+            out = spot;
+        }
+    });
 });
 
 router.get("/GetLot/:companyid/:lotid", async (req, res) => {
     const lot = await model.Lot.find({
         companyid: req.params.companyid,
-        lotid: req.params.lotid
+        lotid: req.params.lotid,
     });
 
     if (lot.length == 0)
@@ -171,7 +189,7 @@ router.get("/GetLot/:companyid/:lotid", async (req, res) => {
 router.get("/GetAllSpots/:companyid/:lotid", async (req, res) => {
     const lot = await model.Lot.find({
         companyid: req.params.companyid,
-        lotid: req.params.lotid
+        lotid: req.params.lotid,
     });
 
     if (lot.length == 0)
@@ -180,8 +198,11 @@ router.get("/GetAllSpots/:companyid/:lotid", async (req, res) => {
     res.send(lot[0].spots);
 });
 
-router.get('/GetPeakTimes/:companyid/:lotid', checkJwt, async(req, res) => {
-  const lot = await model.Lot.find({companyid: req.params.companyid, lotid: req.params.lotid});
+router.get("/GetPeakTimes/:companyid/:lotid", checkJwt, async (req, res) => {
+    const lot = await model.Lot.find({
+        companyid: req.params.companyid,
+        lotid: req.params.lotid,
+    });
 
     //check if we found the lot
     if (lot.length == 0)
@@ -190,7 +211,7 @@ router.get('/GetPeakTimes/:companyid/:lotid', checkJwt, async(req, res) => {
     const spots = lot[0]["spots"]; //we are gauranteed that there is only one entry as lotid is unique
 
     let out;
-    spots.map(spot => {
+    spots.map((spot) => {
         if (req.body.spotid == spot.spotid) {
             out = spot;
         }
@@ -203,7 +224,7 @@ router.get('/GetPeakTimes/:companyid/:lotid', checkJwt, async(req, res) => {
 router.get("/GetPeakTimes/:companyid/:lotid", async (req, res) => {
     const lot = await model.Lot.find({
         companyid: req.params.companyid,
-        lotid: req.params.lotid
+        lotid: req.params.lotid,
     });
 
     if (lot.length == 0)
