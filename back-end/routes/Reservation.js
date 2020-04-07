d5f62bd62e6f42643c289747e875d09da2f8c698
stan <knezes1@unlv.nevada.edu>
2020-04-06 20:27:33 -0700

    adding comments to make my life easier if i ever have to look at this file again

--- back-end/routes/Reservation.js
+++ back-end/routes/Reservation.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const model = require('../model/Model');
 
+
+// welcome to mom's spaghetti all over the code already
 router.post('/AddReservation', async (req, res) => {
+    // i think there's a cleaner way to make this from the req.body but idc
     const reservation = new model.Reservation({
         companyid: req.body.companyid,
         lotid: req.body.lotid,
@@ -11,10 +14,15 @@ router.post('/AddReservation', async (req, res) => {
         username: req.body.username,
         expired: req.body.expired
     });
+    // this will collect all the errors that will inevitably occur
     let err = "";
+
+    // if the end is before the start you're probably in a DeLorean
     if(reservation.starttime.getTime() - reservation.endtime.getTime() >= 0){
         err = "Invalid reservation time ";
     }
+
+    // get the company and lot and see if they exist
     const Company = await model.Company.find({companyid: reservation.companyid });
     const Lot = await model.Lot.find({companyid: reservation.companyid, lotid: reservation.lotid});
     if(Company == null){
@@ -23,12 +31,16 @@ router.post('/AddReservation', async (req, res) => {
     if(Lot.length == 0){
         err += "Lot not found ";
     }
+
+    // get the spots array
     const spots = Lot[0]["spots"];
 
+    // get the specific spot in the array by the id
     var spot = spots.find(element => element.spotid == reservation.spotid);
 
-    var conflict = false;
 
+    var conflict = false;
+    // see if there are any other reservations already made that conflict with the one you're tryna save
     spot.reserveddates.forEach(element => {
         if((reservation.starttime >= element.starttime && reservation.starttime <= element.endtime) 
             || (reservation.endtime >= element.starttime && reservation.endtime <= element.endtime)){
@@ -40,18 +52,23 @@ router.post('/AddReservation', async (req, res) => {
         err += "Selected time conflicts with already scheduled reservations";
     }
 
+    // if this is more than 0 there was 1 (or more) no no's
     if(err.length > 0){
         res.send(err);
     }
     else{
-
+        // update the array that is a field in the schema using 
+        // mongoose weird $ syntax
+        // this hurt my brain
         await model.Lot.update(
             {
+                // find a Lot with these properties 
                 companyid: req.body.companyid,
                 lotid: req.body.lotid,
                 'spots.spotid': req.body.spotid
             },
             {
+                // add to the array of the spot with the same spotid the reservation
                 $addToSet:{
                     'spots.$.reserveddates' : {
                         _id: reservation.id,
@@ -71,6 +88,7 @@ router.post('/AddReservation', async (req, res) => {
             }
         );
 
+        // save the actual reservation
         await reservation.save().then(res =>{
         }).catch(err =>{
             console.log(err);
@@ -81,18 +99,18 @@ router.post('/AddReservation', async (req, res) => {
 });
 
 router.get('/GetReservations/:username', async (req, res) => {
+    // get reservations by customer's username
     let customerReservations = await model.Reservation.find({username: req.params.username});
+    // if it comes back empty, then they got no reservations
     if(customerReservations.length == 0){
-        console.log("1");
         return res.send("No reservations found for customer");
     }
     else{
        try{
-           console.log("2");
+            // else send the list of reservations
            res.send(customerReservations);
        } 
        catch(err){
-           console.log("asedfsdf");
            res.status(500).send(err);
        }
     }
@@ -100,8 +118,10 @@ router.get('/GetReservations/:username', async (req, res) => {
 
 router.delete('/CancelReservation', async (req, res) => {
     try{
+        //find the reservation
         let res = await model.Reservation.findById(req.body.reservationid);
 
+        // remove that reservation time from the lot's spot's array
         await model.Lot.update(
             {
                 companyid: res.companyid,
@@ -109,6 +129,7 @@ router.delete('/CancelReservation', async (req, res) => {
                 'spots.spotid': res.spotid
             },
             {
+                // in the spot, find in it's reservations array the datetime wrapper object that has the same start and end time
                 'spots.$.reserveddates': {
                      $pull: {
                         starttime: req.body.starttime,
@@ -127,7 +148,7 @@ router.delete('/CancelReservation', async (req, res) => {
             }
         )
         
-        console.log("before delete");
+        // find the actual reservation and delete it
         await model.Reservation.findByIdAndDelete(req.body.reservationid);
     } catch(err){
         res.send("Error cancelling reservation");
