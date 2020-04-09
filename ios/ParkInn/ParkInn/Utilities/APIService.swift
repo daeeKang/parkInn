707c25d73558ac4b76abba80da9528ad0f2dec1c
Kyle Aquino <kyleaquino1@gmail.com>
2020-04-08 22:27:37 -0700

    Added a generic function for performing a request to prevent code repetition later on

--- ios/ParkInn/ParkInn/Utilities/APIService.swift
+++ ios/ParkInn/ParkInn/Utilities/APIService.swift
@@ -11,9 +11,16 @@ import Alamofire
 
 class APIService {
     typealias LotsCompletion = (Result<[Lot], AFError>) -> ()
-    static func getLots(companyID: String, completion: @escaping LotsCompletion) {
-        AF.request(APIRouter.companyLots(companyID: companyID)).responseDecodable { (response: DataResponse<[Lot], AFError>) in
-            completion(response.result)
+
+    @discardableResult
+    private static func performRequest<T:Decodable>(route:APIRouter, decoder: JSONDecoder = JSONDecoder(), completion:@escaping (Result<T, AFError>)->Void) -> DataRequest {
+        return AF.request(route)
+                        .responseDecodable (decoder: decoder){ (response: DataResponse<T, AFError>) in
+                            completion(response.result)
         }
+    }
+
+    static func getLots(companyID: String, completion: @escaping LotsCompletion) {
+        performRequest(route: APIRouter.companyLots(companyID: companyID), completion: completion)
     }
 }
