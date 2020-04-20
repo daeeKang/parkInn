b0d4d3b1b536dae70ff72fd9bf400cdacee81ce9
Kyle Aquino <kyleaquino1@gmail.com>
2020-04-19 21:14:34 -0700

    WIP: Getting the directions for map view in LotVC

--- ios/ParkInn/ParkInn/Controllers/User-Facing/LotVC.swift
+++ ios/ParkInn/ParkInn/Controllers/User-Facing/LotVC.swift
@@ -22,12 +22,17 @@ class LotVC: UIViewController {
     @IBOutlet weak var reserveButton: UIButton!
 
     var lot: Lot!
+    let locationManager = CLLocationManager()
+    private let regionZoom: Double = 1000
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         populateView()
         setBarChart()
+        setupMapView()
+        verifyLocationService()
+        getDirections()
 
 // REFACTOR LATER
         peakTimesGraph.layer.cornerRadius = 12.0
@@ -35,6 +40,14 @@ class LotVC: UIViewController {
         mapView.layer.cornerRadius = 12.0
         reserveButton.layer.cornerRadius = reserveButton.frame.height / 2
 
+    }
+
+    private func setupMapView() {
+        // Register the custom annotation
+        mapView.register(LotMarkerView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
+
+        let lotAnnotation = LotAnnotation(lot: lot)
+        mapView.addAnnotation(lotAnnotation)
     }
 
     private func populateView() {
@@ -56,6 +69,36 @@ class LotVC: UIViewController {
                 lotImage.load(url: imageURL)
             }
         }
+    }
+
+    private func getDirections() {
+        guard let location = locationManager.location?.coordinate else { return }
+
+        let request = createDirectionsRequest(from: location)
+        let directions = MKDirections(request: request)
+
+        directions.calculate { [unowned self] (response, error) in
+            guard let response = response else { return }
+
+            for route in response.routes {
+                self.mapView.addOverlay(route.polyline)
+                let mapRect = self.mapView.mapRectThatFits(route.polyline.boundingMapRect, edgePadding: UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20))
+                self.mapView.setVisibleMapRect(mapRect, animated: false)
+            }
+        }
+    }
+
+    func createDirectionsRequest(from coordinate: CLLocationCoordinate2D) -> MKDirections.Request {
+        let startingLocation = MKPlacemark(coordinate: coordinate)
+        let destinationLocation = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: lot.location.latitude, longitude: lot.location.longitude))
+
+        let request = MKDirections.Request()
+        request.source =  MKMapItem(placemark: startingLocation)
+        request.destination = MKMapItem(placemark: destinationLocation)
+        request.transportType = .automobile
+        request.requestsAlternateRoutes = false
+
+        return request
     }
 
     func setBarChart() {
@@ -123,5 +166,86 @@ class LotVC: UIViewController {
 
         mapItem.name = nameLabel.text!
         mapItem.openInMaps(launchOptions: options)
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
+extension LotVC: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // Check if there are any valid locations
+        guard let location = locations.last else { return }
+//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+//        centerMap(at: center, zoom: regionZoom)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // Check the authorizations again
+        verifyLocationAuthorization()
+    }
+
+    private func centerMapOnUser() {
+        if let location = locationManager.location?.coordinate {
+            centerMap(at: location, zoom: regionZoom)
+        }
+    }
+
+    private func centerMap(at center: CLLocationCoordinate2D, zoom: Double) {
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: zoom, longitudinalMeters: zoom)
+        mapView.setRegion(region, animated: true)
+    }
+
+    private func verifyLocationService() {
+        if CLLocationManager.locationServicesEnabled() {
+            // Setup Location Manager
+            setupLocationManager()
+            verifyLocationAuthorization()
+        } else {
+            // Display Alert for no location
+        }
+    }
+
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = .greatestFiniteMagnitude
+
+    }
+
+    private func verifyLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+            case .notDetermined:
+                // Ask for permission
+                locationManager.requestWhenInUseAuthorization()
+                break
+            case .restricted:
+                // Display that their permissions are restricted
+                break
+            case .denied:
+                // Display alert notifying that location services are disabled
+                break
+            case .authorizedAlways, .authorizedWhenInUse:
+                // Location services available - Begin Updating Location
+                locationManager.startUpdatingLocation()
+
+                // Center the map on the user
+                centerMapOnUser()
+                break
+            @unknown default:
+                fatalError("Verify Location returned with an unknown type")
+        }
+    }
+}
+
+// MARK: - MKMapViewDelegate
+
+extension LotVC: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        return nil
+    }
+
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay as! MKPolyline)
+        renderer.strokeColor = .systemBlue
+        return renderer
     }
 }
