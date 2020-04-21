aebd34011a6a6898c4923491188534761220749a
Kyle Aquino <kyleaquino1@gmail.com>
2020-04-20 20:04:48 -0700

    WIP: Beginning to change the UI of main page

--- ios/ParkInn/ParkInn/Controllers/MainVC.swift
+++ ios/ParkInn/ParkInn/Controllers/MainVC.swift
@@ -11,23 +11,24 @@ import MaterialComponents
 
 class MainVC: UIViewController {
 
-    @IBOutlet weak var statsButton: MDCButton!
-    @IBOutlet weak var managementButton: MDCButton!
     @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    private var categories = [Category]()
 
     private let cornerRadius: CGFloat = 12.0
+
+    fileprivate var dataSource: UICollectionViewDiffableDataSource<Section, Category>! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        roundCorners()
-        setupMenu()
-    }
+        setupCategories()
 
-    // Rounds the Corners of the buttons
-    private func roundCorners() {
-        statsButton.layer.cornerRadius = self.cornerRadius
-        managementButton.layer.cornerRadius = self.cornerRadius
+        configureHierarchy()
+        configureDataSource()
+
+        setupMenu()
     }
 
     // Sets up the gestures and action for Menu button
@@ -38,6 +39,18 @@ class MainVC: UIViewController {
         // Add gestures for panning to open/close and tap to close
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+    private func setupCategories() {
+        let events = Category(name: "Events", image: UIImage(systemName: "calendar")!, color: UIColor.PIColors.yellowCardHeader)
+        let revenue = Category(name: "Revenue", image: UIImage(systemName: "dollarsign.square.fill")!, color: UIColor.PIColors.greenCardHeader)
+        let incidents = Category(name: "Incidents", image: UIImage(systemName: "bell.fill")!, color: UIColor.PIColors.redCardHeader)
+        let manage = Category(name: "Manage", image: UIImage(systemName: "car.fill")!, color: UIColor.PIColors.tealCardHeader)
+        let stats = Category(name: "Statistics", image: UIImage(systemName: "chart.pie.fill")!, color: UIColor.PIColors.greenCardHeader)
+
+
+//        categories.append(contentsOf: [manage, revenue, events, incidents])
+        categories.append(contentsOf: [stats, manage])
     }
 
     // MARK: - Segues
@@ -54,5 +67,69 @@ class MainVC: UIViewController {
         if revealViewController().isOpen {
             revealViewController()?.revealToggle(animated: true)
         }
+    }
+}
+
+
+// MARK: - UICollectionViewDataSource/Delegate
+
+extension MainVC: UICollectionViewDelegate {
+
+}
+
+// MARK: - Diffable Datasource and UICollectionViewFlowLayout
+extension MainVC {
+
+    func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalWidth(0.33))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                       subitem: item, count: 2)
+        let spacing = CGFloat(10)
+        group.interItemSpacing = .fixed(spacing)
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = spacing
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+
+    private func configureHierarchy() {
+        collectionView.delegate = self
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .clear
+        collectionView.collectionViewLayout = createLayout()
+        let nib = UINib(nibName: "CategoryCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: "CategoryCell")
+    }
+
+    private func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Category>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, category) -> UICollectionViewCell? in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoryCell", for: indexPath) as? CategoryCell else { fatalError("Could not create new cell") }
+
+            cell.configureCell(with: category)
+
+            return cell
+        })
+
+        // initial data
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Category>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(categories)
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+
+}
+
+extension MainVC {
+    fileprivate enum Section {
+        case main
     }
 }
---
+++ ios/ParkInn/ParkInn/Views/CategoryCell.swift
@@ -0,0 +1,50 @@
+//
+//  CategoryCell.swift
+//  ParkInn
+//
+//  Created by Kyle Aquino on 4/20/20.
+//  Copyright © 2020 ParkInn. All rights reserved.
+//
+
+import UIKit
+import MaterialComponents.MDCBaseCell
+
+class CategoryCell: MDCBaseCell {
+
+    @IBOutlet weak var squareView: UIView!
+    @IBOutlet weak var categoryGlyph: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var cardView: UIView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        self.formatViews()
+        self.clipsToBounds = false
+        self.layer.masksToBounds = false
+    }
+
+    public func configureCell(with category: Category) {
+        categoryLabel.text = category.name
+        categoryGlyph.image = category.image
+        squareView.backgroundColor = category.color
+    }
+
+    private func formatViews() {
+        // Corner Radii
+        squareView.layer.cornerRadius = 5.0
+        cardView.layer.cornerRadius = 5.0
+
+        // Shadows
+        squareView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        squareView.layer.shadowOpacity = 0.3
+        squareView.layer.shadowRadius = 5.0
+        squareView.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+
+        cardView.layer.shadowOffset = CGSize(width: 0, height: 1.5)
+        cardView.layer.shadowOpacity = 0.4
+        cardView.layer.shadowRadius = 3.0
+        cardView.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    }
+
+}
