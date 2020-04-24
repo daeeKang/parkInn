6dc6ff0ea56a4c7499f573a1168a21c170a46c5e
awexis :3 <aolaes@chirotouch.com>
2020-04-23 17:33:24 -0700

    converting stats to functional component

--- front-end/park-inn/src/Components/StatisticsPage/Statistics.js
+++ front-end/park-inn/src/Components/StatisticsPage/Statistics.js
@@ -21,7 +21,7 @@ import {
   peakHoursChart,
 } from './../ChartData/Charts';
 
-import * as statsData from '../../utils/statisticsApi';
+import { getTotalRevenue } from '../../utils/statisticsApi';
 
 // icons
 import MoneyIcon from '../Icons/money_white.svg';
@@ -29,212 +29,211 @@ import UserIcon from '../Icons/users_white.svg';
 import ClockIcon from '../Icons/clock_white.svg';
 import ArrowIcon from '../Icons/arrow.svg';
 
-export default class Statistics extends Component {
-  async componentWillMount() {
-    await this.getLotUtil();
-    this.setState({
-      totalRev: await statsData.getTotalRevenue(),
-      peakHoursData: {
-        series: [
-          await Promise.all([
-            statsData.getPeakTimesCount(12),
-            statsData.getPeakTimesCount(15),
-            statsData.getPeakTimesCount(18),
-            statsData.getPeakTimesCount(21),
-            statsData.getPeakTimesCount(0),
-            statsData.getPeakTimesCount(3),
-            statsData.getPeakTimesCount(6),
-            statsData.getPeakTimesCount(9),
-          ]),
-        ],
-      },
-      averageTimeParked: await statsData.getAverageTimeParked(),
-      // user: await statsData.UserData(),
-    });
-  }
+export const Statistics = props => {
+  // async componentWillMount() {
+  //   await this.getLotUtil();
+  //   this.setState({
+  //     totalRev: await statsData.getTotalRevenue(),
+  //     peakHoursData: {
+  //       series: [
+  //         await Promise.all([
+  //           statsData.getPeakTimesCount(12),
+  //           statsData.getPeakTimesCount(15),
+  //           statsData.getPeakTimesCount(18),
+  //           statsData.getPeakTimesCount(21),
+  //           statsData.getPeakTimesCount(0),
+  //           statsData.getPeakTimesCount(3),
+  //           statsData.getPeakTimesCount(6),
+  //           statsData.getPeakTimesCount(9),
+  //         ]),
+  //       ],
+  //     },
+  //     averageTimeParked: await statsData.getAverageTimeParked(),
+  //     // user: await statsData.UserData(),
+  //   });
+  // }
 
-  async getLotUtil() {
-    const data = await statsData.getLotStatistics();
-    let totalUsersParked = data[0].totalSpots - data[0].availableSpots;
-    this.setState({
-      parked: totalUsersParked.toString(),
-    });
-  }
+  // async getLotUtil() {
+  //   const data = await statsData.getLotStatistics();
+  //   let totalUsersParked = data[0].totalSpots - data[0].availableSpots;
+  //   this.setState({
+  //     parked: totalUsersParked.toString(),
+  //   });
+  // }
 
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      totalRev: '$0.00',
-      peakHoursData: {
-        labels: ['12pm', '3pm', '6pm', '9pm', '12am', '3am', '6am', '9am'],
-        series: [[]],
-      },
-      parked: '0',
-      averageTimeParked: 0,
-      user: '',
-    };
-  }
+  //   this.state = {
+  //     totalRev: '$0.00',
+  //     peakHoursData: {
+  //       labels: ['12pm', '3pm', '6pm', '9pm', '12am', '3am', '6am', '9am'],
+  //       series: [[]],
+  //     },
+  //     parked: '0',
+  //     averageTimeParked: 0,
+  //     user: '',
+  //   };
+  // }
 
-  render() {
-    return (
-      <div>
-        <GridContainer>
-          <GridItem xs={12} sm={6} md={3}>
-            <Card>
-              <CardHeader color="lightTeal" icon>
-                <CardIcon color="lightTeal">
-                  <img src={ClockIcon} height="30px" width="30px" alt="money" />
-                </CardIcon>
-                <p id="icon-subtitle">Average Time Parked</p>
-                <p id="icon-text">
-                  <font color="#74c9d4">
-                    <b>{this.state.averageTimeParked} minute(s)</b>
-                  </font>
-                </p>
-                <Divider />
-                hi
-              </CardHeader>
-            </Card>
-          </GridItem>
-
-          <GridItem xs={12} sm={6} md={3}>
-            <Card>
-              <CardHeader color="red" icon>
-                <CardIcon color="red">
-                  <img src={UserIcon} height="30px" width="30px" alt="money" />
-                </CardIcon>
-                <p id="icon-text">
-                  <font color="#E57373">
-                    <b>{this.state.parked}</b>
-                  </font>{' '}
-                  parked
-                </p>
-                <Divider />
-                <NavLink className="link" to="/parking">
-                  <p id="link">
-                    <img
-                      src={ArrowIcon}
-                      height="12px"
-                      width="12 px"
-                      alt="press-link"
-                    />{' '}
-                    View Lot Utilization
-                  </p>
-                </NavLink>
-              </CardHeader>
-            </Card>
-          </GridItem>
-
-          <GridItem xs={12} sm={6} md={3}>
-            <Card>
-              <CardHeader color="green" icon>
-                <CardIcon color="green">
-                  <img src={MoneyIcon} height="30px" width="30px" alt="money" />
-                </CardIcon>
-                <p id="icon-text">
-                  <font color="#81C784">
-                    <b>$1,000</b>
-                  </font>{' '}
-                  earned today
-                </p>
-                <Divider />
-                <NavLink className="link" to="/parking">
-                  <p id="link">
-                    <img
-                      src={ArrowIcon}
-                      height="12px"
-                      width="12 px"
-                      alt="press-link"
-                    />{' '}
-                    View Sold Spots
-                  </p>
-                </NavLink>
-              </CardHeader>
-            </Card>
-          </GridItem>
-
-          <GridItem xs={12} sm={6} md={3}>
-            <Card style={{ height: '9rem' }}>
-              <CardBody>
-                <CardHeader color="darkBlue">
-                  <h2 id="card-title">
-                    <center>Total Revenue</center>
-                  </h2>
-                </CardHeader>
-                <br />
-                <Divider />
-                <h2 id="card-text">
-                  <center>{this.state.totalRev}</center>
-                </h2>
-              </CardBody>
-            </Card>
-          </GridItem>
-
-          <GridItem xs={12} sm={6} md={4}>
-            <Card>
-              <CardHeader color="lightBlue">
-                <ChartistGraph
-                  className="ct-chart-line"
-                  data={dailySalesChart.data}
-                  type={'Line'}
-                  options={dailySalesChart.options}
-                  listener={dailySalesChart.animation}
-                />
-              </CardHeader>
-              <CardBody>
-                <Divider />
-                <br />
-                <font color="#90CAF9" id="card-text">
-                  This Week's Sales Trend
+  // render() {
+  const { totalRevenue } = getTotalRevenue();
+  console.log('totalRevenue :>> ', totalRevenue);
+  return (
+    <div>
+      <GridContainer>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="lightTeal" icon>
+              <CardIcon color="lightTeal">
+                <img src={ClockIcon} height="30px" width="30px" alt="money" />
+              </CardIcon>
+              <p id="icon-subtitle">Average Time Parked</p>
+              <p id="icon-text">
+                <font color="#74c9d4">
+                  {/* <b>averageTimeParked minute(s)</b> */}
                 </font>
-              </CardBody>
-            </Card>
-          </GridItem>
+              </p>
+              <Divider />
+              hi
+            </CardHeader>
+          </Card>
+        </GridItem>
 
-          <GridItem xs={12} sm={6} md={4}>
-            <Card>
-              <CardHeader color="blue">
-                <ChartistGraph
-                  className="ct-chart-line"
-                  data={monthlySalesChart.data}
-                  type={'Line'}
-                  options={monthlySalesChart.options}
-                  listener={monthlySalesChart.animation}
-                />
-              </CardHeader>
-              <CardBody>
-                <Divider />
-                <br />
-                <font color="#4ea3cc" id="card-text">
-                  Monthly Revenue Earned
-                </font>
-              </CardBody>
-            </Card>
-          </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="red" icon>
+              <CardIcon color="red">
+                <img src={UserIcon} height="30px" width="30px" alt="money" />
+              </CardIcon>
+              <p id="icon-text">
+                <font color="#E57373">{/* <b>parked</b> */}</font> parked
+              </p>
+              <Divider />
+              <NavLink className="link" to="/parking">
+                <p id="link">
+                  <img
+                    src={ArrowIcon}
+                    height="12px"
+                    width="12 px"
+                    alt="press-link"
+                  />{' '}
+                  View Lot Utilization
+                </p>
+              </NavLink>
+            </CardHeader>
+          </Card>
+        </GridItem>
 
-          <GridItem xs={12} sm={6} md={4}>
-            <Card>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="green" icon>
+              <CardIcon color="green">
+                <img src={MoneyIcon} height="30px" width="30px" alt="money" />
+              </CardIcon>
+              <p id="icon-text">
+                <font color="#81C784">
+                  <b>$1,000</b>
+                </font>{' '}
+                earned today
+              </p>
+              <Divider />
+              <NavLink className="link" to="/parking">
+                <p id="link">
+                  <img
+                    src={ArrowIcon}
+                    height="12px"
+                    width="12 px"
+                    alt="press-link"
+                  />{' '}
+                  View Sold Spots
+                </p>
+              </NavLink>
+            </CardHeader>
+          </Card>
+        </GridItem>
+
+        <GridItem xs={12} sm={6} md={3}>
+          <Card style={{ height: '9rem' }}>
+            <CardBody>
               <CardHeader color="darkBlue">
-                <ChartistGraph
-                  className="ct-chart-bar"
-                  data={this.state.peakHoursData}
-                  type={'Bar'}
-                  options={peakHoursChart.options}
-                  listener={peakHoursChart.animation}
-                />
+                <h2 id="card-title">
+                  <center>Total Revenue</center>
+                </h2>
               </CardHeader>
-              <CardBody>
-                <Divider />
-                <br />
-                <font color="#1B262C" id="card-text">
-                  Peak Hours
-                </font>
-              </CardBody>
-            </Card>
-          </GridItem>
-        </GridContainer>
-      </div>
-    );
-  }
-}
+              <br />
+              <Divider />
+              <h2 id="card-text">{/* <center>totalRev</center> */}</h2>
+            </CardBody>
+          </Card>
+        </GridItem>
+
+        <GridItem xs={12} sm={6} md={4}>
+          <Card>
+            <CardHeader color="lightBlue">
+              <ChartistGraph
+                className="ct-chart-line"
+                data={dailySalesChart.data}
+                type={'Line'}
+                options={dailySalesChart.options}
+                listener={dailySalesChart.animation}
+              />
+            </CardHeader>
+            <CardBody>
+              <Divider />
+              <br />
+              <font color="#90CAF9" id="card-text">
+                This Week's Sales Trend
+              </font>
+            </CardBody>
+          </Card>
+        </GridItem>
+
+        <GridItem xs={12} sm={6} md={4}>
+          <Card>
+            <CardHeader color="blue">
+              <ChartistGraph
+                className="ct-chart-line"
+                data={monthlySalesChart.data}
+                type={'Line'}
+                options={monthlySalesChart.options}
+                listener={monthlySalesChart.animation}
+              />
+            </CardHeader>
+            <CardBody>
+              <Divider />
+              <br />
+              <font color="#4ea3cc" id="card-text">
+                Monthly Revenue Earned
+              </font>
+            </CardBody>
+          </Card>
+        </GridItem>
+
+        <GridItem xs={12} sm={6} md={4}>
+          <Card>
+            <CardHeader color="darkBlue">
+              <ChartistGraph
+                className="ct-chart-bar"
+                // data=peakHoursData
+                type={'Bar'}
+                options={peakHoursChart.options}
+                listener={peakHoursChart.animation}
+              />
+            </CardHeader>
+            <CardBody>
+              <Divider />
+              <br />
+              <font color="#1B262C" id="card-text">
+                Peak Hours
+              </font>
+            </CardBody>
+          </Card>
+        </GridItem>
+      </GridContainer>
+    </div>
+  );
+  // }
+};
+
+export default Statistics;
--- front-end/park-inn/src/utils/statisticsApi.js
+++ front-end/park-inn/src/utils/statisticsApi.js
@@ -1,44 +1,84 @@
 import api from './api';
 
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-const companyName = 'MGM'; // temp
+import { useAuth0 } from '../react-auth0-spa';
 
-export const getTotalRevenue = async () => {
+function UserData() {
+  const { user } = useAuth0();
+  // const companyName = user.companyName; // temp
+  return user;
+}
+
+export const getTotalRevenue = async Component => {
   try {
-    const { data } = await api.get(`Statistic/${companyName}`);
+    const user = UserData();
+    const { data } = await api.get(`Statistic/${user.companyName}`);
+    console.log('user :>> ', user);
+    console.log('data :>> ', data);
     var rev = data.companyStatistics.revenue;
     rev = rev / 100;
-    return rev.toFixed(2);
+    return props => {
+      const totalRev = rev.toFixed(2);
+      return <Component {...props} totalRevenue={totalRev} />;
+    };
   } catch (err) {
-    console.log(err.message);
+    console.log(err);
   }
 };
 
-export const getPeakTimesCount = async index => {
-  try {
-    const { data } = await api.get(`Statistic/${companyName}`);
-    return data.companyStatistics.peakTimes[index].count;
-  } catch (err) {
-    console.log(err.message);
-  }
-};
+// export const getTotalRevenue = async () => {
+//   try {
+//     const user = UserData();
+//     const { data } = await api.get(`Statistic/${user.companyName}`);
+//     var rev = data.companyStatistics.revenue;
+//     rev = rev / 100;
+//     const totalRevenue = rev.toFixed(2);
+//     return totalRevenue;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
-export const getLotStatistics = async index => {
-  try {
-    const { data } = await api.get(`Statistic/${companyName}`);
-    return data.lotStatistics;
-  } catch (err) {
-    console.log(err.message);
-  }
-};
+// export const getTotalRevenue = async () => {
+//   try {
+//     const user = userData();
+//     const { data } = await api.get(`Statistic/${user.companyName}`);
+//     var rev = data.companyStatistics.revenue;
+//     rev = rev / 100;
+//     return rev.toFixed(2);
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// };
 
-export const getAverageTimeParked = async (index = 0) => {
-  try {
-    const { data } = await api.get(`Statistic/${companyName}`);
-    return data.lotStatistics[index].averageTimeParked.currentAverage;
-  } catch (err) {
-    console.log(err.message);
-  }
-};
+// export const getPeakTimesCount = async index => {
+//   try {
+//     const user = userData();
+//     const { data } = await api.get(`Statistic/${user.companyName}`);
+//     return data.companyStatistics.peakTimes[index].count;
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// };
+
+// export const getLotStatistics = async index => {
+//   try {
+//     const user = userData();
+//     const { data } = await api.get(`Statistic/${user.companyName}`);
+//     return data.lotStatistics;
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// };
+
+// export const getAverageTimeParked = async (index = 0) => {
+//   try {
+//     const user = userData();
+//     const { data } = await api.get(`Statistic/${user.companyName}`);
+//     return data.lotStatistics[index].averageTimeParked.currentAverage;
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// };
