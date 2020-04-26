b04cb6e8cd511dd740bc1d9cd980b63ef60564f2
Lichelle Gaerlan <lichellemaegaerlan@gmail.com>
2020-04-25 22:05:33 -0700

    put password button and add employee form inside expansion panel

--- front-end/park-inn/src/Components/ManagementPage/Management.js
+++ front-end/park-inn/src/Components/ManagementPage/Management.js
@@ -8,7 +8,30 @@ import Loading from '../LoadingPage/Loading';
 import 'react-notifications/lib/notifications.css';
 import {NotificationContainer, NotificationManager} from 'react-notifications';
 
+import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+}));
+
 const Management = () => {
+    const classes = useStyles();
     const { loading, user } = useAuth0();
     
     if (loading) {
@@ -33,7 +56,42 @@ const Management = () => {
 
 
     return (
-      <div class="page">
+      <div>
+        <center>Account Settings</center>
+        <br/><br/>
+        <NotificationContainer />
+        <div className={classes.root}>
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+          <Typography className={classes.heading}>General Settings</Typography>
+          <Typography className={classes.secondaryHeading}>Change your password</Typography>
+          </ExpansionPanelSummary>
+
+          <ExpansionPanelDetails>
+            <button class="button change-password"  onClick={handlePasswordChange}>Change Password</button>
+          </ExpansionPanelDetails>
+      </ExpansionPanel>
+      
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography className={classes.heading}>Employee Settings</Typography>
+          <Typography className={classes.secondaryHeading}>Add employees to manage parking</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <AddEmployee></AddEmployee>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
+
+      {/* <div class="page">
         <NotificationContainer />
         <div class="page-header">Account Management</div>
         <div class="management-container">
@@ -42,7 +100,9 @@ const Management = () => {
             <AddEmployee></AddEmployee>
           </div>
         </div>
-      </div>
+      </div> */}
+    </div>
+  
     );
 }
 
